171773b65bc6511acf4170013a310d0f388718d0
Carlos Asparrin Martin <150694554+SujetoDelta-XXI@users.noreply.github.com>
2025-09-13 22:15:09 -0500

    a

--- mi-sitio-express/controllers/pokedexController.js
+++ mi-sitio-express/controllers/pokedexController.js
@@ -1,15 +1,12 @@
-// "Base de datos" en memoria
 let pokedex = [];
 
 const list = (req, res) => {
-  // 👇 Aquí enviamos la variable pokedex a la vista
   res.render("pokedex", { pokedex });
 };
 
 const save = (req, res) => {
   const { nombre, tipo, peso, shiny, oscuro, imagen } = req.body;
 
-  // Agregamos el nuevo registro al array
   pokedex.push({
     nombre,
     tipo,
@@ -19,7 +16,6 @@ const save = (req, res) => {
     imagen
   });
 
-  // Redirigimos a la lista
   res.redirect("/pokedex");
 };
 
